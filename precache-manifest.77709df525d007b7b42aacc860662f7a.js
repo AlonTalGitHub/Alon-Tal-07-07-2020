d8e68d92b2529e8b46bd7d59515ad90783803087
Alon Tal <alontal23@gmail.com>
2020-07-13 16:37:05 +0300

    Updates

---
+++ precache-manifest.77709df525d007b7b42aacc860662f7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "642b7cc46dcc9df31f461650232f4fdf",
+    "url": "/Alon-Tal-07-07-2020/index.html"
+  },
+  {
+    "revision": "ce68f7a300260f493fb9",
+    "url": "/Alon-Tal-07-07-2020/static/js/2.019e7d12.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/Alon-Tal-07-07-2020/static/js/2.019e7d12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07736e76d59737a0c597",
+    "url": "/Alon-Tal-07-07-2020/static/js/main.4a534ac1.chunk.js"
+  },
+  {
+    "revision": "49518530962520f00004",
+    "url": "/Alon-Tal-07-07-2020/static/js/runtime-main.aeaf3be1.js"
+  },
+  {
+    "revision": "127d6b3dd0ba04112bd87916c59640f3",
+    "url": "/Alon-Tal-07-07-2020/static/media/NeueHaasDisplay-Roman.127d6b3d.woff"
+  }
+]);
