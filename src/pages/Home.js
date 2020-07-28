01b396c22461efa8d93f90e745155a084376ccf3
Alon Tal <alontal23@gmail.com>
2020-07-28 08:56:39 +0300

    UI updates and add debounce

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,6 @@ import { connect } from "react-redux";
 
 import Grid from "@material-ui/core/Grid";
 import TextField from "@material-ui/core/TextField";
-import Button from "@material-ui/core/Button";
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import { makeStyles } from "@material-ui/core/styles";
 import { 
